447ffc5131c09f88df378467ea1658a543999e69
Austin Hendricks <hendraustin@gmail.com>
2024-02-20 22:01:16 -0800

    Update className

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -2,7 +2,7 @@ import styles from "./sections.module.css";
 
 export default function About() {
   return (
-    <div id="about" className={styles.about_section}>
+    <div id="about" className={styles.about}>
       <p>
         Hi there, I'm Austin. I began this journey at the end of 2022 after graduating from Oregon
         State University-Cascades in Bend, Oregon.
