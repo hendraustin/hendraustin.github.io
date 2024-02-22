43d7baa0b8fdfd999b7db0f4171f90229bd4a33a
Austin Hendricks <hendraustin@gmail.com>
2024-02-21 16:25:04 -0800

    Fill out about section

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -4,8 +4,20 @@ export default function About() {
   return (
     <div id="about" className={styles.about}>
       <p>
-        Hi there, I'm Austin. I began this journey at the end of 2022 after graduating from Oregon
-        State University-Cascades in Bend, Oregon.
+        Hi there, I'm Austin. My objective is to leverage my technical skills in a role that
+        contributes to the improvement of society. I am driven by the idea of encountering
+        opportunities where I can make a positive impact and actively engage in projects that serve
+        the greater good.
+      </p>
+      <p>
+        My current goal is to find a fullstack position with a company that aligns with my personal
+        mission statement at a tangible level. I've been drawn to TypeScript and React.js lately,
+        with this site being built with Next.js, however, I am not averse to diving into backend
+        business logic.
+      </p>
+      <p>
+        Outside of my professional pursuits, my hobbies include reading, a passion nurtured during
+        my five-year employment at a bookstore, as well as rock climbing and disc golf.
       </p>
     </div>
   );
