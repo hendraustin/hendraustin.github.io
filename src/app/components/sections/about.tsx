31071b3de21f57d86e9b63a5e2f73af7b8720683
Austin Hendricks <hendraustin@gmail.com>
2024-02-23 10:34:07 -0800

    Reduce about section

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -5,16 +5,12 @@ export default function About() {
     <div id="about" className={styles.about}>
       <p>
         Hi there, I&apos;m Austin. My objective is to leverage my technical skills in a role that
-        contributes to the improvement of society. I am driven by the idea of encountering
-        opportunities where I can make a positive impact and actively engage in projects that serve
-        the greater good.
+        contributes to the improvement of society. I'm currently lookign to find a fullstack
+        position with a company that aligns with my personal goal at a tangible level. I&apos;ve
+        been drawn to TypeScript and React lately, with this site being built with Next.js, however,
+        I am not averse to diving into backend business logic.
       </p>
-      <p>
-        My current goal is to find a fullstack position with a company that aligns with my personal
-        mission statement at a tangible level. I&apos;ve been drawn to TypeScript and React.js lately,
-        with this site being built with Next.js, however, I am not averse to diving into backend
-        business logic.
-      </p>
+      <p></p>
       <p>
         Outside of my professional pursuits, my hobbies include reading, a passion nurtured during
         my five-year employment at a bookstore, as well as rock climbing and disc golf.
