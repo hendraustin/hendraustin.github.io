aa85dc9054ca396b6b9828522533e8969e07dd68
Austin Hendricks <hendraustin@gmail.com>
2024-02-23 10:36:24 -0800

    Add apostraphe

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <div id="about" className={styles.about}>
       <p>
         Hi there, I&apos;m Austin. My objective is to leverage my technical skills in a role that
-        contributes to the improvement of society. I'm currently lookign to find a fullstack
+        contributes to the improvement of society. I&apos;m currently lookign to find a fullstack
         position with a company that aligns with my personal goal at a tangible level. I&apos;ve
         been drawn to TypeScript and React lately, with this site being built with Next.js, however,
         I am not averse to diving into backend business logic.
