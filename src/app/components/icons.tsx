212ed95e8ef99ee5d32692954054f3c8f0b7e5f3
Austin Hendricks <hendraustin@gmail.com>
2024-02-27 12:12:31 -0800

    Add animation for secret picture

--- src/app/components/icons.tsx
+++ src/app/components/icons.tsx
@@ -1,24 +1,39 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import styles from "./icons.module.css";
 import { FaCat, FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 
 export default function Icons() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleClick = () => {
+    setModalOpen(true);
+  };
+
+  let modal = document.getElementById("modal");
+  modal?.addEventListener("click", function () {
+    this.style.display = "none";
+    setModalOpen(false);
+  });
+
   return (
     <div className={styles.icons}>
       <span></span>
-      <a href="https://github.com/hendraustin" className={styles.icon}>
+      <a href="https://github.com/" className={styles.icon} id="githubIcon">
         <FaGithub />
       </a>
-      <a href="https://www.linkedin.com/in/hendricks-austin/" className={styles.icon}>
+      <a href="https://www.linkedin.com/in/" className={styles.icon}>
         <FaLinkedin />
       </a>
-      <a href="mailto: hendraustin@gmail.com" className={styles.icon}>
+      <a href="mailto: @gmail.com" className={styles.icon}>
         <MdOutlineMail />
       </a>
-      <a href="../../static/pam_cat.jpeg" className={styles.cat_icon}>
+      <a className={styles.cat_icon} onClick={handleClick}>
         <FaCat />
       </a>
+      <div className={styles.modal} id="modal" style={{ display: modalOpen ? "block" : "none" }}>
+        <img className={styles.modal_content} src="../../static/pam_cat.jpeg" />
+      </div>
     </div>
   );
 }
